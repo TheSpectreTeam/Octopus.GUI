a31fa0f1a02d5d63383eb20d3990b8c9c17beec3
Dmitry-Titov-K <titovdmitryk@gmail.com>
2022-05-05 19:51:54 +0300

    feat(theme)[OCT-19]: added basic Chakra theme to project

--- src/app/providers/withChakra.tsx
+++ src/app/providers/withChakra.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { ChakraProvider } from '@chakra-ui/react'
+import theme from "../../theme";
 
 export const withChakra = (component: () => React.ReactNode) => () => 
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.Suspense fallback="...loading">{component()}</React.Suspense>
   </ChakraProvider>;
 
