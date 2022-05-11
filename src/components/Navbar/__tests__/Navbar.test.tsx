3919123fbd5d6b00833b7541567e0db918680d04
Dmitry-Titov-K <titovdmitryk@gmail.com>
2022-05-11 20:34:35 +0300

    feature(Navbar/tests)[OCT-19]: added profile menu after click on Avatar

--- src/components/Navbar/__tests__/Navbar.test.tsx
+++ src/components/Navbar/__tests__/Navbar.test.tsx
@@ -4,26 +4,32 @@ import { screen, render, fireEvent } from "@testing-library/react";
 import Navbar from "..";
 
 describe("Navbar component", () => {
-  it("Сomponent should be rendered", () => {
-    render(<Navbar />);
-    const text = screen.getByRole("heading");
-    expect(text).toBeInTheDocument();
-  });
-  it("should render auth button", () => {
-    render(<Navbar />);
-    const loginButton = screen.getByRole("button");
-    expect(loginButton).toBeInTheDocument();
-  });
-  it("should render Logout button after click to Login button", () => {
-    render(<Navbar />);
+    it("Сomponent should be rendered", () => {
+        render(<Navbar />);
+        const text = screen.getByRole("heading");
+        expect(text).toBeInTheDocument();
+    });
+    it("should render auth button", () => {
+        render(<Navbar />);
+        const loginButton = screen.getByRole("button");
+        expect(loginButton).toBeInTheDocument();
+    });
+    it("should render Avatar and logout button after click to Login button", () => {
+        render(<Navbar />);
 
-    const button = screen.getByRole("button");
+        const button = screen.getByRole("button", { name: /login/i });
 
-    expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent(/login/i);
+        expect(button).toBeInTheDocument();
+        expect(screen.queryByRole("menuitem")).not.toBeInTheDocument();
 
-    fireEvent.click(button);
+        fireEvent.click(button);
 
-    expect(button).toHaveTextContent(/logout/i);
-  });
+        expect(
+            screen.getByRole("button", { name: /avatar/i })
+        ).toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole("button", { name: /avatar/i }));
+
+        expect(screen.getByTestId("logout-btn")).toBeInTheDocument();
+    });
 });
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,57 +1,101 @@
-import { Box, Button, Flex, Heading } from "@chakra-ui/react";
 import React from "react";
+import {
+    Avatar,
+    Box,
+    Button,
+    Flex,
+    Heading,
+    HStack,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Spacer,
+    Stack,
+    Text,
+} from "@chakra-ui/react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-  const [auth, setAuth] = React.useState(false);
+    const [auth, setAuth] = React.useState(false);
 
-  const isAuth = auth;
+    const isAuth = auth;
 
-  const handleLogin = () => {
-    setAuth(true);
-  };
+    const handleLogin = () => {
+        setAuth(true);
+    };
 
-  const handleLogout = () => {
-    setAuth(false);
-  };
-  return (
-    <Box>
-      <Flex
-        height={20}
-        flexDir="row"
-        minWidth={"min-content"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-      >
-        <Flex />
+    const handleLogout = () => {
+        setAuth(false);
+    };
+
+    return (
         <Box>
-          <Heading role="heading">Octopus</Heading>
+            <Flex
+                height={20}
+                flexDir="row"
+                minWidth={"min-content"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+            >
+                <Flex w={"30%"} />
+                <Spacer />
+                <Box>
+                    <Stack align={"center"}>
+                        <Heading as="h5" size={"lg"} role="heading">
+                            Octopus
+                        </Heading>
+                    </Stack>
+                </Box>
+                <Spacer />
+                <Box w={"30%"} mx="2em" alignItems={"center"}>
+                    <Flex alignContent="center" justifyContent={"end"}>
+                        {isAuth ? (
+                            <HStack>
+                                <Menu data-testid='profile-menu'>
+                                    <MenuButton
+                                        as={Button}
+                                        rounded={"full"}
+                                        variant={"link"}
+                                        backgroundSize={"md"}
+                                        cursor={"pointer"}
+                                        minW={"min-content"}
+                                    >
+                                        <Avatar size={"md"} />
+                                    </MenuButton>
+                                    <MenuList>
+                                        <Text
+                                            data-testid="user-name-label"
+                                            align={"center"}
+                                            role="textbox"
+                                            py={3}
+                                        >
+                                            Administrator
+                                        </Text>
+                                        <MenuItem
+                                            data-testid="logout-btn"
+                                            onClick={handleLogout}
+                                        >
+                                            Logout
+                                        </MenuItem>
+                                    </MenuList>
+                                </Menu>
+                            </HStack>
+                        ) : (
+                            <Button
+                                onClick={handleLogin}
+                                variant={"solid"}
+                                colorScheme="blue"
+                            >
+                                Login
+                            </Button>
+                        )}
+                    </Flex>
+                </Box>
+            </Flex>
         </Box>
-        <Box width={"400px"} mx="2em" alignItems={"center"}>
-          <Flex alignContent="center" justifyContent={"center"}>
-            {isAuth ? (
-              <Button
-                onClick={handleLogout}
-                variant={"solid"}
-                colorScheme="blue"
-              >
-                Logout
-              </Button>
-            ) : (
-              <Button
-                onClick={handleLogin}
-                variant={"solid"}
-                colorScheme="blue"
-              >
-                Login
-              </Button>
-            )}
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
-  );
+    );
 };
 
 export default Navbar;
