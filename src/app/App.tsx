f4b282a8b5a758264817ad5124483f89366753a0
PC-DEV\DevAdmin <titovdmitryk@gmail.com>
2022-05-14 17:54:58 +0300

    feat(App/Layout)[OCT-26]: added Sidebar in app layout view

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 import Layout from "../layouts";
 import { withProviders } from "./providers";
 
 const App = () => {
-  return (
-    <main>
-      <Layout />
-    </main>
-  );
+    return (
+        <Layout>
+            <Content />
+        </Layout>
+    );
+};
+
+//TODO: Remove this demo component
+const Content = () => {
+    return <Box height={1500}>CONTENT</Box>;
 };
 
 export default withProviders(App);
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,13 +1,42 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, chakra, Flex } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-const Layout: React.FC = () => {
-  return (
-    <Box minH={"100vh"}>
-      <Navbar />
-    </Box>
-  );
+type Props = {
+    children?: React.ReactNode;
 };
 
+const Layout: React.FC<Props> = ({ children }) => {
+    return (
+        <Box minH={"100vh"}>
+            <Navbar />
+            <Flex flexDirection={{ base: "column", md: "row" }}>
+                <Sidebar>
+                    <NavItems />
+                </Sidebar>
+                <chakra.main
+                    width={"full"}
+                    role={"main"}
+                    padding={"1rem"}
+                    marginLeft={{ base: "none", md: 20 }}
+                >
+                    {children}
+                </chakra.main>
+            </Flex>
+        </Box>
+    );
+};
+//TODO: Remove this demo component
+const NavItems = () => {
+    return (
+        <>
+            {Array(5)
+                .fill(1)
+                .map(() => (
+                    <div>NAV</div>
+                ))}
+        </>
+    );
+};
 export default Layout;
