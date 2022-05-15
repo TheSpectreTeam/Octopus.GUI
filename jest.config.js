d6bf6e54fd722ce413b4dfb8a56392ed7b5a6c58
Dmitry-Titov-K <titovdmitryk@gmail.com>
2022-05-15 22:09:49 +0300

    feat(tests)[OCT-26]: added test-utils and configured jest alias

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', "<rootDir>/mocks.js"]
+    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', "<rootDir>/mocks.js"],
+    moduleDirectories: ['node_modules', "utils", __dirname]
 };
