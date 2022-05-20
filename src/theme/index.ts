3036603a1a9ce3a116801143a25248cdfce485bb
PC-DEV\DevAdmin <titovdmitryk@gmail.com>
2022-05-20 17:31:08 +0300

    feat(theme)[OCT-30]: added customization styles

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,12 +1,30 @@
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: true,
+    initialColorMode: "dark",
+    useSystemColorMode: true,
+};
+
+const colors = {
+    gray: {
+        800: "#1E1E2D",
+        900: "#151521",
+    },
+};
+
+const styles = {
+    global: (props: any) => ({
+        "html, body": {
+            background: mode("gray.50", "gray.900")(props),
+        },
+    }),
 };
 
 const theme = extendTheme({
-  config,
+    config,
+    colors,
+    styles,
 });
 
 export default theme;
